d6ed3e65ce97f633618c80bf0df77c719f57577c
홍민주 <mj09287@gmail.com>
2020-11-13 18:18:35 +0900

    123

---
+++ gudi_project/Travel_connDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+namespace gudi_project
+{
+    public class Travel_conn
+    {
+        public string   trv_ID              {   get; set; }
+        public string   trv_info_ID     {   get; set; }
+    }
+
+    public class Travel_connDB
+    {
+        
+    }
+}
